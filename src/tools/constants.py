7e82bad6605972b9c18186d510d73cdd9eec6c49
Rod Regado <regadorod@gmail.com>
2023-03-01 13:36:23 -0800

    Add kepler_E

---
+++ src/tools/constants.py
@@ -0,0 +1,6 @@
+
+# Earth-related
+g = 9.81 # gravity acceleration
+muE = 398600 # gravitational paramter km3/s2
+
+
--- src/tools/unit_tests/test_orbit_calculations.py
+++ src/tools/unit_tests/test_orbit_calculations.py
@@ -1,13 +1,14 @@
 
 
+# 3rd party libraries
+import math as m
 # local libraries
 import orbit_calculations as oc
-
 
 def test_calc_JD():
     JD = oc.calc_JD("1:1:2010:11:12:4")
 
-    assert JD == 2455197.96671 # 01/01/2010 11:12:4
+    assert m.isclose(JD, 2455197.96671, rel_tol=1e-4) == True # 01/01/2010 11:12:4
 
     # - Julian Date (JD) calc
     # - Local Sideral Time (LST) calc
@@ -19,3 +20,13 @@ def test_calc_JD():
     # - Keplers
     # - Orbit transfers
     # - Pertubations options
+
+def test_kepler_E():
+    e = 0.37255
+    M = 3.6029
+
+    E = oc.kepler_E(e, M)
+    assert m.isclose(E, 3.47942, rel_tol=1e-4) == True
+
+def test_sv2coe():
+    pass
